b41a58ffe0784a15ecd7dfb34d52496c8dd08440
Ewan <ewan.s@cucumberwifi.io>
2020-03-20 10:13:24 +0000

    refactor score formatting

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -5,6 +5,7 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private String[] scores = {"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -20,33 +21,12 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
         if (m_score1 == m_score2) {
             score = drawScore();
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             score = advantageOrWinningScore();
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score += "-";
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
-            }
+            score = scores[m_score1] + "-" + scores[m_score2];
         }
         return score;
     }
